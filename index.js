8d4c9917b7f35d8d1e4a70d1e36aed55222fdcdd
Nahid Sultan <nahidsultan38@gmail.com>
2025-07-24 13:33:01 +0800

    implement server to data fetch.

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const userName = process.env.USER_NAME;
+const userPassword = process.env.USER_PASSWORD;
+
+const uri = `mongodb+srv://${userName}:${userPassword}@cluster0.chdotmq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("serviceDB");
+    const serviceCollection = database.collection("Bookings");
+
+    app.get("/bookings", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/booking/:email", async (req, res) => {
+      const email = req.param.email;
+      const query = { email: email };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await serviceCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete id", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`App ruiing on port: ${port}`);
+});
+// nahid
+// B4XsrE3jePC46T3E
